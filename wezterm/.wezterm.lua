bc60b13cce04ad6287cbb1765f76410c538005cd
mattot <mattot.builder@gmail.com>
2025-08-14 21:44:14 +0800

    feat: updated wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 18
+config.font_size = 16
 
 config.enable_tab_bar = false
 
